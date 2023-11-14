62295b7c460f4cf6d2a5aa4c565de66a55913922
NoumanAshraf0 <139555532+NoumanAshraf0@users.noreply.github.com>
2023-11-14 14:32:02 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// Import required modules
+const express = require('express');
+
+// Create an instance of the Express application
+const app = express();
+
+// Define a route for the homepage
+app.get('/', (req, res) => {
+  res.send('<h1>Hi, This is a demo App!</h1>');
+});
+
+// Define a route for a sample API endpoint
+app.get('/api/data', (req, res) => {
+  const data = {
+    message: 'This is some sample data from the API.',
+    timestamp: new Date(),
+  };
+  res.json(data);
+});
+
+// Set up the server to listen on a port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
